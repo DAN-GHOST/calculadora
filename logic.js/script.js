4788d6c73f92c3d4e13f01581fbbfd4ce67b0d26
Daniel Puentes <dpuentesf1@ucentral.edu.co>
2023-12-21 15:54:51 -0500

    Se creo la logica de funcionamiento para la calculadora solamente. ES pertiente mencionar que es funcional y que es capas de extraer los números y el simbolo de operación para realizar el proceso

---
+++ logic.js/script.js
@@ -0,0 +1,65 @@
+function sumar(a,b){
+    return a+b;
+}
+
+function restar(a,b){
+    return a-b;
+}
+
+function dividir(a,b){
+    return a/b;
+}
+
+function multiplicar(a,b){
+    return a*b;
+}
+
+let operacion = prompt("Por favor, ingresa la operación que quieres realizar. +, -, x, /");
+let arr = operacion.split('');
+
+let simbolo = null;
+let arr_number_one = [];
+let arr_number_two = [];
+let getNumberOne = null;
+let getNumbertwo = null;
+
+for(let i= 0; i < arr.length; i++){
+    if(arr[i] === ' '){
+        arr.splice(i,1);
+        --i;
+    }
+}
+
+for(let i= 0; i < arr.length; i++){
+    if(arr[i] === '+' || arr[i] === '-' || arr[i] === '*' || arr[i] === '/'){
+        simbolo = arr[i];
+    }else{
+        if(simbolo === null){
+            arr_number_one.push(arr[i]);
+        }else{
+            arr_number_two.push(arr[i]);
+        }
+    }
+
+}
+
+getNumberOne = +(arr_number_one.join(''));
+getNumbertwo = +(arr_number_two.join(''));
+
+switch(simbolo){
+    case '+':
+        alert(sumar(getNumberOne,getNumbertwo));
+    break;
+
+    case '-':
+        alert(restar(getNumberOne,getNumbertwo));
+    break;
+
+    case '*':
+        alert(multiplicar(getNumberOne,getNumbertwo));
+    break;
+
+    case '/':
+        alert(dividir(getNumberOne,getNumbertwo));
+    break;
+}
